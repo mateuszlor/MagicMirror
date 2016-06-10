cdf72826c3c584460c9aaf679b78db5a7b1b97cc
Mateusz Lorencki <Mateusz Lorencki>
2016-06-10 18:37:37 +0200

    Bluetooth webservice

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # MagicMirror 
 
+## Weather
+
 If THE WEATHER section is not working, please generate the API key via following site: openweathermap.org
 (Sign up and the key will be generated automatically). Then, paste it here - APPID: 'YOUR_KEY' in config.js file.
+
+## Bluetooth discovery webservice
+
+### Prerequisites
+- Python 2.7
+- Compatible bluetooth adapter
+
+### Instalation
+```
+cd python
+pip instal pybluez
+easy_install web.py
+python inoutboard.py
+```
+
+### API usage
+
+- GET localhost:8080/discover - devices discovery
+- GET localhost:8080/one - closest device
---
+++ python/inoutboard.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import bluetooth
+import time
+import web
+
+urls = (
+    '/discover', 'discover',
+    '/one', 'get_one'
+)
+
+app = web.application(urls, globals())
+
+class discover:        
+    def GET(self):
+        try:
+            nearby_devices = bluetooth.discover_devices(lookup_names = True)
+            output = '[';
+            for device in nearby_devices:
+                print "Found device with address: ",  device[0], " and name: ", device[1]
+                output += '{address:' + device[0] + ', name: ' + device[1] + '},'
+            output += ']';
+            return output
+        except Exception, e:
+            print "Unexpected error: ", e
+            return "ERROR"
+
+class get_one:
+    def GET(self):
+        try:
+            nearby_devices = bluetooth.discover_devices(lookup_names = True)
+            output = '[';
+            for device in nearby_devices:
+                print "Found device with address: ",  device[0], " and name: ", device[1]
+                output += '{address:' + device[0] + ', name: ' + device[1] + '},'
+            output += ']';
+            return output
+        except Exception, e:
+            print "Unexpected error: ", e
+            return "ERROR"
+
+if __name__ == "__main__":
+    app.run()
+
