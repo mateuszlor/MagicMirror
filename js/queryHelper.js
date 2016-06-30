e91d0abe9953f268e3912fc4e0f3a8888142072d
Mateusz Lorencki <Mateusz Lorencki>
2016-06-30 10:27:53 +0200

    Query helper fix for no params

--- js/queryHelper.js
+++ js/queryHelper.js
@@ -5,9 +5,11 @@ var queryHelper = {
 queryHelper.fetchQuery = function() {
 	$.each(document.location.search.substr(1).split('&'), function(c,q) {
     	var i = q.split('=');
-    	queryHelper.queries[i[0].toString()] = i[1].toString();
+    	if(i.length >= 2)
+    	{
+    		queryHelper.queries[i[0].toString()] = i[1].toString();
+    	}
   });
   console.log(queryHelper.queries);
-  return queryHelper.queries
 }
   
