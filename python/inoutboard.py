d27cd0e19fed0fef71fb1802a53b6c89ff9b0f31
Mateusz Lorencki <Mateusz Lorencki>
2016-07-05 00:20:25 +0200

    !! EXPERIMENTAL !! Discover only know devices

--- python/inoutboard.py
+++ python/inoutboard.py
@@ -18,8 +18,11 @@ class discover:
             nearby_devices = bluetooth.discover_devices(lookup_names = True)
             output = '[';
             for device in nearby_devices:
-                print "Found device with address: ",  device[0], " and name: ", device[1]
-                output += "{\"address\":\"" + device[0] + "\", \"name\": \"" + device[1] + "\"},"
+                if device[0] == "FA:75:46:03:37:1E" or device[0] == "2C:54:CF:4A:82:E7" or device[0] == "CC:07:AB:8B:EB:56":
+                    print "Found device with address: ",  device[0], " and name: ", device[1]
+                    output += "{\"address\":\"" + device[0] + "\", \"name\": \"" + device[1] + "\"},"
+                else:
+                    print "UNKNOWN device with address: ",  device[0], " and name: ", device[1]
             output = output[:-1];
             output += ']';
             return output
